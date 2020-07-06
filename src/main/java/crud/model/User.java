e88cd15ab814d11a07534a33f997aa41a6e53092
Gary <startex@ya.ru>
2020-07-06 22:52:58 +0300

    Initial commit

---
+++ src/main/java/crud/model/User.java
@@ -0,0 +1,88 @@
+package crud.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String position, int age, String email) {
+        this.name = name;
+        this.position = position;
+        this.age = age;
+        this.email = email;
+    }
+
+//    public Long getId() {
+    public Integer getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
